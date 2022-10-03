f3585d25b7a687eb3050ffaa9eebfc7c64d59c6f
jdrb2098 <juan.rondon.bonilla@gmail.com>
2022-10-03 15:16:27 -0500

    db enviroment variables & postgres conection

--- backend/settings.py
+++ backend/settings.py
@@ -133,13 +133,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('BIA_DB_NAME'),
+        'USER': os.environ.get('BIA_DB_USER'),
+        'PASSWORD': os.environ.get('BIA_DB_PASSWORD'),
+        'HOST': os.environ.get('BIA_DB_HOST'),
+        'PORT': os.environ.get('BIA_DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
