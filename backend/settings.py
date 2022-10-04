2916fb50bedfc8a15133c8f8133f00cd3a225228
jdrb2098 <juan.rondon.bonilla@gmail.com>
2022-10-04 11:41:18 -0500

    delete backend-bia folder

--- backend/settings.py
+++ backend/settings.py
@@ -80,7 +80,7 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
     'USER_ID_FIELD': 'id_usuario',
-    'USER_ID_CLAIM': '_id',
+    'USER_ID_CLAIM': 'user_id',
 
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_TYPE_CLAIM': 'token_type',
