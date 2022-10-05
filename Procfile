12836497b9671c7011469f3a6045c45f3a847096
jdrb2098 <juan.rondon.bonilla@gmail.com>
2022-10-05 18:19:22 -0500

    deployment config v 0.1

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn backend-bia.wsgi
+web: python manage.py migrate && gunicorn ISAMC_Knowledge.wsgi
--- backend/settings.py
+++ backend/settings.py
@@ -113,7 +113,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'frontend/build')
+            os.path.join(BASE_DIR, 'build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -188,7 +188,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build/static') #coneccion a los statics IMPOPRTANTEEEEE....!
+    os.path.join(BASE_DIR, 'build/static') #coneccion a los statics IMPOPRTANTEEEEE....!
 ]
 
 
