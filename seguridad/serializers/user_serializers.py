c1198a2e45bb9893de94b79a3def18bca7b7d0f7
jdrb2098 <juan.rondon.bonilla@gmail.com>
2022-10-05 15:20:59 -0500

    serializers update

--- seguridad/serializers/user_serializers.py
+++ seguridad/serializers/user_serializers.py
@@ -2,15 +2,40 @@
 from dataclasses import field
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
-from seguridad.models import User, UsuariosRol
+from seguridad.models import User, UsuariosRol, HistoricoActivacion
+from seguridad.serializers.personas_serializers import PersonasSerializer
+
+class HistoricoActivacionSerializers(serializers.ModelSerializer):
+    class Meta:
+        model= HistoricoActivacion
+        fields = '__all__'
+
+class UserRolesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UsuariosRol
+        fields = '__all__'
+class UsuarioCreadorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
     _id = serializers.SerializerMethodField(read_only=True)
     isAdmin = serializers.SerializerMethodField(read_only=True)
+    id_usuario_creador = UsuarioCreadorSerializer(read_only=True)
+    persona = PersonasSerializer(read_only=True)
+    usuario_rol = UserRolesSerializer(read_only=True)
+    
 
     class Meta:
         model = User
-        fields = ['id_usuario', '_id', 'nombre_de_usuario', 'email', 'isAdmin']
+        fields = '__all__'
+
+    def get_usuario_rol(self, obj):
+        rol = obj.usuariosrol_set.all()
+        serializer = UserRolesSerializer(rol, many=True)
+        return serializer.data
 
     def get__id(self, obj):
         return obj.id_usuario
@@ -18,6 +43,17 @@ class UserSerializer(serializers.ModelSerializer):
     def get_isAdmin(self, obj):
         return obj.is_staff
 
+    def get_usuario_creador(self,obj):
+        usuario_creador= obj.id_usuario_creador
+        serializer = UsuarioCreadorSerializer(usuario_creador,many=True)
+        return serializer.data
+    
+    def create(self, validated_data):
+        usuario_creador = validated_data.pop('usuario_creador')
+        user_instance = User.objects.create(**validated_data)
+        for user in usuario_creador:
+            User.objects.create(user=user_instance,**user)
+        return user_instance
 
 
 class UserSerializerWithToken(UserSerializer):
@@ -30,9 +66,3 @@ class UserSerializerWithToken(UserSerializer):
     def get_token(self, obj):
         token = RefreshToken.for_user(obj)
         return str(token.access_token)
-
-class UserRoles(serializers.ModelSerializer):
-
-    class Meta:
-        model = UsuariosRol
-        fields = '__all__'
