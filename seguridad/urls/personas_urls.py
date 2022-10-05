86eed9018be98af30ad8d2bba782f9fd5f0bb8f3
rbenprieto <rbenprieto@outlook.com>
2022-10-05 10:02:58 -0500

    tipo documento views & urls

--- seguridad/urls/personas_urls.py
+++ seguridad/urls/personas_urls.py
@@ -3,11 +3,18 @@ from seguridad.views import personas_views as views
 
 
 urlpatterns = [
-    path('registerestadocivil', views.registerEstadoCivil, name='estado-civil-register'), 
-    path('getestadocivil', views.getEstadoCivil, name="estado-civil-get"),
+    # Estado Civil
+    path('registerestadocivil/', views.registerEstadoCivil, name='estado-civil-register'), 
+    path('getestadocivil/', views.getEstadoCivil, name="estado-civil-get"),
     path('updateestadocivil/<str:pk>/', views.updateEstadoCivil, name='estado-civil-update'),
     path('deleteestadocivil/<str:pk>/', views.deleteEstadoCivil, name='estado-civil-delete'),
     path('getestadocivil/<str:pk>/', views.getEstadoCivilById, name='estado-civil-id-get'),
 
+    # Tipo Documento
+    path('registertipodocumento/', views.registerTipoDocumento, name='tipo-documento-register'), 
+    path('gettipodocumento/', views.getTipoDocumento, name="tipo-documento-get"),
+    path('gettipodocumento/<str:pk>/', views.getTipoDocumentoById, name='tipo-documento-id-get'),
+    path('updatetipodocumento/<str:pk>/', views.updateTipoDocumento, name='tipo-documento-update'),
+    path('deletetipodocumento/<str:pk>/', views.deleteTipoDocumento, name='tipo-documento-delete'),
 
 ]
