5e6af346c636bb1312c291ed3d559a9b82c7cc00
Fabian Ramirez <fabianramirez.ingenieria@gmail.com>
2022-10-06 14:09:42 -0500

    Refactoriación zerializers y views de permisos

--- seguridad/serializers/permisos_serializers.py
+++ seguridad/serializers/permisos_serializers.py
@@ -4,20 +4,25 @@ from seguridad.models import (
     PermisosModulo, 
     PermisosModuloRol,
 )
-
+from seguridad.serializers.auditorias_serializers import ModulosSerializers
+from .roles_serializers import RolesSerializer
 
 class PermisosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Permisos
         fields = '__all__'
         
-
 class PermisosModuloSerializer(serializers.ModelSerializer):
+    id_modulo = ModulosSerializers(read_only=True)
+    cod_permiso = PermisosSerializer(read_only=True)    
     class Meta:
         model = PermisosModulo
         fields = '__all__'
-    
+        
 class PermisosModuloRolSerializer(serializers.ModelSerializer):
+    cod_permiso = PermisosSerializer(read_only=True)
+    id_rol = RolesSerializer(read_only=True)
+    id_modulo = ModulosSerializers(read_only=True)
     class Meta:
         model = PermisosModuloRol
         fields = '__all__'
