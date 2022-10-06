20a47768cc9f5f69a4a2261b1714d60d88beef68
jdrb2098 <juan.rondon.bonilla@gmail.com>
2022-10-05 19:21:48 -0500

    fixes

--- seguridad/models.py
+++ seguridad/models.py
@@ -1,8 +1,3 @@
-from email.policy import default
-from random import choices
-from tkinter.tix import Tree
-from turtle import mode
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from .managers import CustomUserManager
