072e0fcabe98416c4b8611e6cac4d7b78232d957
jdrb2098 <juan.rondon.bonilla@gmail.com>
2022-10-06 18:00:55 -0500

    user urls conflict

--- seguridad/urls/user_urls.py
+++ seguridad/urls/user_urls.py
@@ -3,6 +3,7 @@ from seguridad.views import user_views as views
 
 
 urlpatterns = [
+    
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 
     path('register/', views.RegisterView.as_view(), name='register'),
@@ -10,6 +11,8 @@ urlpatterns = [
     path('profile/', views.getUserProfile, name="users-profile"),
     path('profile/update/', views.updateUserProfile, name="user-profile-update"), 
    
-    path('roles/', views.roles, name='roles'), 
+    path('roles/', views.roles, name='roles'),
+    path("", views.getUsers, name="get-users"),
+    path("<str:pk>/", views.getUsers, name="get-users"), 
 
 ]
