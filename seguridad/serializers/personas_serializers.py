c97fd456d8003ba50e4124640bd96c940a214f2e
rbenprieto <rbenprieto@outlook.com>
2022-10-06 08:54:21 -0500

    personas views/serializers/urls

--- seguridad/serializers/personas_serializers.py
+++ seguridad/serializers/personas_serializers.py
@@ -23,45 +23,65 @@ class TipoDocumentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = TipoDocumento
         fields = '__all__'
-        
-        
+
+
+class RepresentanteLegalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Personas
+        fields = '__all__'
+
 class PersonasSerializer(serializers.ModelSerializer):
-    #tipoDocumento = serializers.SerializerMethodField(read_only=True)
+    tipo_documento = TipoDocumentoSerializer(read_only=True)
+    estado_civil = EstadoCivilSerializer(read_only=True)
+    representante_legal = RepresentanteLegalSerializer(read_only=True)
         
     class Meta:
         model = Personas
         fields = '__all__'
         
-    def get_tipoDocumento(self, instance):
-        response = super().to_representation(instance)
-        response['tipo_documento'] = TipoDocumentoSerializer(instance.tipo_documento).data
-        return response
+    def get_tipo_documento(self, obj):
+        tipo_documento = obj.tipodocumento_set.all()
+        serializer = TipoDocumentoSerializer(tipo_documento, many=True)
+        return serializer.data
     
-    def get_estadoCivil(self, instance):
-        response = super().to_representation(instance)
-        response['estado_civil'] = EstadoCivilSerializer(instance.estado_civil).data
-        return response
+    def get_estado_civil(self, obj):
+        estado_civil = obj.estadocivil_set.all()
+        serializer = EstadoCivilSerializer(estado_civil, many=True)
+        return serializer.data
+    
+    def get_representante_legal(self, obj):
+        representante_legal = obj.representantelegal_set.all()
+        serializer = RepresentanteLegalSerializer(representante_legal, many=True)
+        return serializer.data
            
 
 class ApoderadoPersonaSerializer(serializers.ModelSerializer):
+    persona_poderdante = PersonasSerializer(read_only=True)
+    persona_apoderada = PersonasSerializer(read_only=True)
+    
     class Meta:
         model = ApoderadoPersona
         fields = '__all__'
         
         
 class SucursalesEmpresasSerializer(serializers.ModelSerializer):
+    id_empresa = PersonasSerializer(read_only=True)
+    
     class Meta:
         model = SucursalesEmpresas
         fields = '__all__'
         
 
 class HistoricoEmailsSerializer(serializers.ModelSerializer):
+    id_persona = PersonasSerializer(read_only=True)
     class Meta:
         model = HistoricoEmails
         fields = '__all__'
         
         
 class HistoricoDireccionSerializer(serializers.ModelSerializer):
+    id_persona = PersonasSerializer(read_only=True)
+    
     class Meta:
         model = HistoricoDireccion
         fields = '__all__'
@@ -74,6 +94,8 @@ class ClasesTerceroSerializer(serializers.ModelSerializer):
         
         
 class ClasesTerceroPersonaSerializer(serializers.ModelSerializer):
+    id_clase_tercero = ClasesTerceroSerializer(read_only=True)
+    id_persona = PersonasSerializer(read_only=True)
     class Meta:
         model = ClasesTerceroPersona
         fields = '__all__'
--- seguridad/urls/personas_urls.py
+++ seguridad/urls/personas_urls.py
@@ -3,24 +3,56 @@ from seguridad.views import personas_views as views
 
 
 urlpatterns = [
-    # Estado Civil
-    path('registerestadocivil/', views.registerEstadoCivil, name='estado-civil-register'), 
-    path('getestadocivil/', views.getEstadoCivil, name="estado-civil-get"),
-    path('updateestadocivil/<str:pk>/', views.updateEstadoCivil, name='estado-civil-update'),
-    path('deleteestadocivil/<str:pk>/', views.deleteEstadoCivil, name='estado-civil-delete'),
+    
+    # Estado Civil 
+    path('getestadoscivil/', views.getEstadoCivil, name="estado-civil-get"),
     path('getestadocivil/<str:pk>/', views.getEstadoCivilById, name='estado-civil-id-get'),
+    path('deleteestadocivil/<str:pk>/', views.deleteEstadoCivil, name='estado-civil-delete'),
+    path('updateestadocivil/<str:pk>/', views.updateEstadoCivil, name='estado-civil-update'),
+    path('registerestadocivil/', views.registerEstadoCivil, name='estado-civil-register'),
     
-    # Tipo Documento
-    path('registertipodocumento/', views.registerTipoDocumento, name='tipo-documento-register'), 
-    path('gettipodocumento/', views.getTipoDocumento, name="tipo-documento-get"),
-    path('updatetipodocumento/<str:pk>/', views.updateTipoDocumento, name='tipo-documento-update'),
-    path('deletetipodocumento/<str:pk>/', views.deleteTipoDocumento, name='tipo-documento-delete'),
+    # Tipo Documento 
+    path('gettiposdocumento/', views.getTipoDocumento, name="tipo-documento-get"),
     path('gettipodocumento/<str:pk>/', views.getTipoDocumentoById, name='tipo-documento-id-get'),
-    
-    # Personas
-    path('registerpersona/', views.registerPersona, name='persona-register'), 
-    path('getpersonas/', views.getPersonas, name="personas-get"),
-    path('updatepersona/<str:pk>/', views.updatePersona, name='persona-update'),
     path('deletetipodocumento/<str:pk>/', views.deleteTipoDocumento, name='tipo-documento-delete'),
+    #path('updatetipodocumento/<str:pk>/', views.updateTipoDocumento, name='tipo-documento-update'),
+    #path('registertipodocumento/', views.registerTipoDocumento, name='tipo-documento-register'),
+    
+    # Personas 
+    path('getpersonas/', views.getPersonas, name="personas-get"),
     path('getpersona/<str:pk>/', views.getPersonaById, name='persona-id-get'),
+    path('deletepersona/<str:pk>/', views.deletePersona, name='persona-delete'),
+    #path('updatepersona/<str:pk>/', views.updatePersona, name='persona-update'),
+    #path('registerpersona/', views.registerPersona, name='persona-register'),
+    
+    # Apoderados Personas
+    path('getapoderadospersonas/', views.getApoderadosPersona, name="apoderados-personas-get"),
+    path('getapoderadopersona/<str:pk>/', views.getApoderadoPersonaById, name='apoderado-persona-id-get'),
+    path('deleteapoderadopersona/<str:pk>/', views.deleteApoderadoPersona, name='apoderado-persona-delete'),
+    
+    # Sucursales Empresas
+    path('getsucursalesempresas/', views.getSucursalesEmpresas, name="sucursales-empresas-get"),
+    path('getsucursalempresa/<str:pk>/', views.getSucursalEmpresaById, name='sucursal-empresa-id-get'),
+    path('deletesucursalempresa/<str:pk>/', views.deleteSucursalEmpresa, name='sucursal-empresa-delete'),
+    
+    # Historico Emails
+    path('gethistoricoemails/', views.getHistoricoEmails, name="historico-emails-get"),
+    path('gethistoricoemail/<str:pk>/', views.getHistoricoEmailById, name='historico-email-id-get'),
+    path('deletehistoricoemail/<str:pk>/', views.deleteHistoricoEmail, name='historico-email-delete'),
+    
+    # Historico Direcciones
+    path('gethistoricodirecciones/', views.getHistoricoDirecciones, name="historico-direcciones-get"),
+    path('gethistoricodireccion/<str:pk>/', views.getHistoricoDireccionById, name='historico-direccion-id-get'),
+    path('deletehistoricodireccion/<str:pk>/', views.deleteHistoricoDireccion, name='historico-direccion-delete'),
+    
+    # Clases Tercero
+    path('getclasestercero/', views.getClasesTercero, name="clases-tercero-get"),
+    path('getclasetercero/<str:pk>/', views.getClaseTerceroById, name='clase-tercero-id-get'),
+    path('deleteclasetercero/<str:pk>/', views.deleteClaseTercero, name='clase-tercero-delete'),
+    
+    # Clases Tercero
+    path('getclasesterceropersonas/', views.getClasesTerceroPersonas, name="clases-tercero-personas-get"),
+    path('getclaseterceropersona/<str:pk>/', views.getClaseTerceroPersonaById, name='clase-tercero-persona-id-get'),
+    path('deleteclaseterceropersona/<str:pk>/', views.deleteClaseTerceroPersona, name='clase-tercero-persona-delete'),
+
 ]
