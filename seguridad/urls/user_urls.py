454de5b439f7cbaff76fc1f946c12762c992194b
rbenprieto <rbenprieto@outlook.com>
2022-10-06 14:59:29 -0500

    review serializers/views/urls

--- seguridad/urls/user_urls.py
+++ seguridad/urls/user_urls.py
@@ -3,19 +3,18 @@ from seguridad.views import user_views as views
 
 
 urlpatterns = [
-    path('login/', views.MyTokenObtainPairView.as_view(),
-         name='token_obtain_pair'),
-
-    path('register/', views.registerUser, name='register'),
-
-    path('profile/', views.getUserProfile, name="users-profile"),
-    path('profile/update/', views.updateUserProfile, name="user-profile-update"),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    
+    # User
+    
     path('', views.getUsers, name="users"),
-    path('roles/', views.roles, name='roles'),
-
     path('<str:pk>/', views.getUserById, name='user'),
-
+    path('delete/<str:pk>/', views.deleteUser, name='user-delete'),
     path('update/<str:pk>/', views.updateUser, name='user-update'),
-
-    path('delete/<str:pk>/', views.deleteUser, name='user-delete'),  
+    path('register/', views.registerUser, name='register'),
+    path('profile/', views.getUserProfile, name="users-profile"),
+    path('profile/update/', views.updateUserProfile, name="user-profile-update"), 
+    
+    # Roles
+    path('roles/', views.roles, name='roles'), 
 ]
