1a062aa575745ab5433ad3f46df28c152c44e0fe
kamilo arias <youtube_.1034@hotmail.com>
2022-10-28 18:06:41 -0500

    add models almacen, marcas, EstadoArticulos, and auditorias

--- seguridad/views/personas_views.py
+++ seguridad/views/personas_views.py
@@ -581,7 +581,6 @@ class RegisterPersonaJuridica(generics.CreateAPIView):
             
             return Response({'status': status.HTTP_201_CREATED, 'detail': serializer.data})
 
-
 # Views for apoderados persona
 
 
@@ -629,17 +628,25 @@ class deleteSucursalEmpresa(generics.DestroyAPIView):
     
     def delete(self,request,pk):
         sucursal=SucursalesEmpresas.objects.filter(id_sucursal_empresa=pk).first()
+
         if sucursal:
+            persona_empresa=sucursal.id_persona_empresa
             sucursal.delete()
-            usuario=request.user.id_usuario
-            user = User.objects.get(id_usuario = usuario)
-            modulo = Modulos.objects.get(id_modulo = 1)
-            permiso = Permisos.objects.get(cod_permiso = 'BO')
+            persona=Personas.objects.get(id_persona=persona_empresa.id_persona)
+            usuario = request.user.id_usuario
+            dirip = Util.get_client_ip(request)
+            descripcion ={ "nombre razón social": str(persona.razon_social),"sucursal" :str(sucursal.sucursal)}
+            auditoria_data = {
+                'id_usuario': usuario,
+                'id_modulo': 1,
+                'cod_permiso': 'BO',
+                'subsistema': 'TRSV',
+                'dirip': dirip,
+                'descripcion': descripcion,
+            }
             
-            descripcion = "Sucursal:" + str(pk)+ "Sucursal:" + str(sucursal.sucursal)+ "."
-            direccion=Util.get_client_ip(request)
-            Auditorias.objects.create(id_usuario = user, id_modulo = modulo, id_cod_permiso_accion = permiso, subsistema = "TRSV", dirip=direccion, descripcion=descripcion)  
-            #headers = self.get_success_headers(sucursal.data)
+            Util.save_auditoria(auditoria_data)
+
             return Response({'detail':'la sucursal empresa fue eliminada'})
         else:
             return Response({'detail':'No existe sucursal'})
@@ -657,22 +664,24 @@ class updateSucursalEmpresa(generics.RetrieveUpdateAPIView):
             sucursal_serializer = self.serializer_class(sucursal, data=request.data)
             sucursal_serializer.is_valid(raise_exception=True)
             sucursal_serializer.save()
-            usuario=request.user.id_usuario
-            user = User.objects.get(id_usuario = usuario)
-            modulo = Modulos.objects.get(id_modulo = 1)
-            permiso = Permisos.objects.get(cod_permiso = 'AC')
             
-            valores_actualizados = ""
-            del previous_sucursal.__dict__["_state"]
-            del previous_sucursal.__dict__["_django_version"]  
-            for field, value in previous_sucursal.__dict__.items():
-                    new_value = getattr(sucursal,field)
-                    if value != new_value:
-                        valores_actualizados += field + "_anterior:" + str(value) + ";" + field + "_nuevo:" + str(new_value) + ";"
-                        
-            descripcion = "Sucursal id:" + str(sucursal.id_sucursal_empresa)+ "Sucursal:" + str(sucursal.sucursal)+ "."
-            direccion=Util.get_client_ip(request)
-            Auditorias.objects.create(id_usuario = user, id_modulo = modulo, id_cod_permiso_accion = permiso, subsistema = "TRSV", dirip=direccion, descripcion=descripcion, valores_actualizados=valores_actualizados)  
+            usuario = request.user.id_usuario
+            persona=Personas.objects.get(id_persona=request.data['id_persona_empresa'])
+            dirip = Util.get_client_ip(request)
+            descripcion ={ "nombre razón social": str(persona.razon_social),"sucursal" :str(sucursal.sucursal)}
+            valores_actualizados={'current':sucursal, 'previous':previous_sucursal}
+
+            auditoria_data = {
+                'id_usuario': usuario,
+                'id_modulo': 1,
+                'cod_permiso': 'AC',
+                'subsistema': 'TRSV',
+                'dirip': dirip,
+                'descripcion': descripcion,
+                'valores_actualizados': valores_actualizados
+            }
+            
+            Util.save_auditoria(auditoria_data)
             return Response({'detail':'la sucursal empresa actualizada'})
         else:
             return Response({'detail':'No existe sucursal'})
@@ -688,17 +697,22 @@ class registerSucursalEmpresa(generics.CreateAPIView):
 
         serializador=serializer.save()
         usuario = request.user.id_usuario
-        user = User.objects.get(id_usuario = usuario)
-        modulo = Modulos.objects.get(id_modulo = 1)
-        permiso = Permisos.objects.get(cod_permiso = 'CR')
+
+        persona=Personas.objects.get(id_persona=request.data['id_persona_empresa'])
+        dirip = Util.get_client_ip(request)
+        descripcion ={ "nombre razón social": str(persona.razon_social),"sucursal" :str(serializador.sucursal)}
+
+
+        auditoria_data = {
+            'id_usuario': usuario,
+            'id_modulo': 1,
+            'cod_permiso': 'CR',
+            'subsistema': 'TRSV',
+            'dirip': dirip,
+            'descripcion': descripcion,
+        }
         
-        currentdate = datetime.date.today()
-        formatDate = currentdate.strftime("%d/%m/%y")
-        
-        
-        descripcion = "Sucursal:" + str(serializador.pk)+  ";" + "fecha:" + formatDate + ";" + "observaciones:Registro de otra sucursal" + ";" + "NumeroSucursal:"+ str(serializador.numero_sucursal) + "Sucursal:" + str(serializador.sucursal)+"."
-        direccion=Util.get_client_ip(request)
-        Auditorias.objects.create(id_usuario = user, id_modulo = modulo, id_cod_permiso_accion = permiso, subsistema = "TRSV", dirip=direccion, descripcion=descripcion)  
+        Util.save_auditoria(auditoria_data)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
     
