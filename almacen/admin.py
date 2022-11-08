2b1c7f8571d7880d9cc77cb8e2c87c856c40a95c
jdrb2098 <juan.rondon.bonilla@gmail.com>
2022-11-08 14:29:12 -0500

    almacen varios

--- almacen/admin.py
+++ almacen/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from almacen.models.organigrama_models import (
+    Organigramas,
+    NivelesOrganigrama,
+    UnidadesOrganizacionales,
+    Cargos
+)
 
-# Register your models here.
+admin.site.register(Organigramas)
+admin.site.register(NivelesOrganigrama)
+admin.site.register(UnidadesOrganizacionales)
+admin.site.register(Cargos)
--- almacen/models/organigrama_models.py
+++ almacen/models/organigrama_models.py
@@ -1,1 +1,82 @@
 from django.db import models
+
+
+class Organigramas(models.Model):
+    id_organigrama = models.AutoField(primary_key=True, editable=False, db_column='T017IdOrganigrama')
+    nombre = models.CharField(max_length=50, unique=True, db_column='T017nombre')
+    fecha_terminado = models.DateTimeField(auto_now_add=True, null=True, blank=True,db_column='T017fechaTerminado')
+    descripcion = models.CharField(max_length=255, db_column='T017descripcion')
+    fecha_puesta_produccion = models.DateField(null=True, blank=True, db_column='T017fechaPuestaEnProduccion')
+    fecha_retiro_produccion = models.DateField(null=True, blank=True, db_column='T017fechaRetiroDeProduccion')
+    justificacion_nueva_version = models.CharField(max_length=255, null=True, blank=True, db_column='T017justificacionNuevaVersion')
+    version = models.CharField(max_length=10, db_column='T017version')
+    actual = models.BooleanField(default=False, db_column='T017actual')
+    ruta_resolucion = models.CharField(max_length=255, null=True, blank=True, db_column='T017rutaResolucion')
+    
+    def __str__(self):
+        return str(self.nombre)
+
+    class Meta:
+        db_table = 'T017Organigramas'
+        verbose_name = 'Organigrama'
+        verbose_name_plural = 'Organigramas'
+          
+
+class NivelesOrganigrama(models.Model):
+    id_nivel_organigrama = models.AutoField(primary_key = True, editable=False, db_column='T018IdNivelOrganigrama')
+    id_organigrama = models.ForeignKey(Organigramas, on_delete=models.CASCADE, null=False, blank=False, db_column='T018Id_Organigrama')
+    orden_nivel = models.SmallIntegerField(db_column='T018ordenDelNivel', unique=True)
+    nombre = models.CharField(max_length=50, db_column='T018nombre', unique=True)
+    
+    def __str__(self):
+        return str(self.nombre)
+
+    class Meta:
+        db_table = 'T018NivelesOrgnigrama'
+        verbose_name = 'NivelOrganigrama'
+        verbose_name_plurar = 'NivelesOrganigrama'
+        unique_together = ['id_organigrama', 'orden_nivel']
+        unique_together = ['id_organigrama', 'nombre']
+
+
+class UnidadesOrganizacionales(models.model):
+    unidades_organizacional=models.AutoField(primary_key=True,editable=False,db_column='T019IdUnidadOrganizacional')
+    id_organigrama=models.ForeignKey(Organigramas, on_delete=models.CASCADE, db_column='T019Id_Organigrama')
+    id_nivel_organigrama=models.ForeignKey(NivelesOrganigrama, on_delete=models.CASCADE, db_column='T019Id_NivelOrganigrama')
+    nombre=models.models.CharField(max_length=50, db_column='T019nombre')
+    codigo=models.CharField(max_length=10,db_column='T019codigo')
+    cod_tipo_unidad=models.CharField(max_length=2,db_column='T019codTipoUnidad')
+    cod_agrupacion_documental=models.CharField(max_length=3,db_column='T019codTipoUnidad')
+    unidad_raiz=models.BooleanField(db_column='T019unidadRaiz',default=False)
+    id_unidad_org_padre=models.ForeignKey('self',on_delete=models.SET_NULL,blank=True,null=True,db_column='T019Id_UnidadOrgPadre')
+
+    def __str__(self):
+        return str(self.nombre) 
+    class Meta:
+        db_table='T019UnidadesOrganizacionales'
+        verbose_name= 'Unidad organizacional'
+        verbose_name_plural= 'Unidad organizacionales'
+        unique_together = ['id_organigrama', 'nombre']
+        unique_together = ['id_organigrama', 'codigo']
+
+
+class Cargos(models.Model):
+    id_cargo = models.AutoField(primary_key=True, editable=False, db_column='T009IdCargo')
+    nombre = models.CharField(max_length=50, unique=True, db_column='T009nombre')
+    activo = models.BooleanField(default=False, db_column='T009activo')
+    item_usado = models.BooleanField(default=False, db_column='T009itemYaUsado')
+    
+    def __str__(self):
+        return str(self.nombre)
+        
+    class Meta:
+        db_table = 'T009Cargos'
+        verbose_name = 'Cargo'
+        verbose_name_plural = 'Cargos'
+
+
+
+
+
+
+
