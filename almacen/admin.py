8cecc2603380c3df6dbb3c3fd5fdd903160575a9
rbenprieto <rbenprieto@outlook.com>
2022-11-10 09:07:58 -0500

    detail persona in bodega

--- almacen/admin.py
+++ almacen/admin.py
@@ -5,8 +5,19 @@ from almacen.models.organigrama_models import (
     UnidadesOrganizacionales,
     Cargos
 )
+from almacen.models.generics_models import (
+    Marcas,
+    PorcentajesIVA,
+    UnidadesMedida,
+    Bodegas
+
+)
 
 admin.site.register(Organigramas)
 admin.site.register(NivelesOrganigrama)
 admin.site.register(UnidadesOrganizacionales)
 admin.site.register(Cargos)
+admin.site.register(Marcas)
+admin.site.register(PorcentajesIVA)
+admin.site.register(UnidadesMedida)
+admin.site.register(Bodegas)
