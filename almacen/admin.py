e0c49273d5737a1b87231c541e8c5000c976c858
Fabian Ramirez <fabianramirez.ingenieria@gmail.com>
2022-11-21 09:55:55 -0500

    Se agregan las tablas de almacen al admin de django

--- almacen/admin.py
+++ almacen/admin.py
@@ -17,6 +17,23 @@ from almacen.models.generics_models import (
     UnidadesMedida,
     Bodegas
 )
+from almacen.models.articulos_models import (
+    MetodosValoracionArticulos,
+    TiposDepreciacionActivos,
+    TiposActivo
+)
+from almacen.models.hoja_de_vida_models import (
+    VehiculosArrendados,
+    HojaDeVidaComputadores,
+    HojaDeVidaVehiculos,
+    HojaDeVidaOtrosctivos,
+    DocumentosVehiculo
+)
+#from almacen.models.inventario_models import ()
+from almacen.models.mantenimientos_models import (
+    ProgramacionMantenimientos,
+    RegistroMantenimientos
+)
 
 admin.site.register(Organigramas)
 admin.site.register(NivelesOrganigrama)
@@ -30,3 +47,13 @@ admin.site.register(CuadrosClasificacionDocumental)
 admin.site.register(SubseriesDoc)
 admin.site.register(SeriesDoc)
 admin.site.register(SeriesSubseriesUnidadOrg)
+admin.site.register(MetodosValoracionArticulos)
+admin.site.register(TiposDepreciacionActivos)
+admin.site.register(TiposActivo)
+admin.site.register(VehiculosArrendados)
+admin.site.register(HojaDeVidaComputadores)
+admin.site.register(HojaDeVidaVehiculos)
+admin.site.register(HojaDeVidaOtrosctivos)
+admin.site.register(DocumentosVehiculo)
+admin.site.register(ProgramacionMantenimientos)
+admin.site.register(RegistroMantenimientos)
